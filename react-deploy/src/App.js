cf0dcbef397b5ac58cbae9a669596881dde655fb
Shanmathi Ganesan <shanmathi18g@gmail.com>
2024-01-24 05:41:59 +0530

    added files

--- react-deploy/src/App.js
+++ react-deploy/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
-import Home from './components/Home.js'
+// import Home from './components/Home.js'
+import Login from './components/Login.js'
 
 function App() {
   return (
-    
-    <Home/>
+    <>
+    <Login />
+    </>
   );
 }
 
---
+++ react-deploy/src/components/Login.js
@@ -0,0 +1,46 @@
+import logo from './LogoTrans.png';
+
+function Login(){
+    return (
+        <>
+  <title>Sign In</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+  <link href="login.css" rel="stylesheet" />
+  <header>
+    <img src={logo} height={75} width={210} />
+    <nav>
+      <a href="premain.html" className="aboutus">
+        About Us
+      </a>
+      <form action="register.html">
+        <button className="register">Register</button>
+      </form>
+    </nav>
+  </header>
+  <div className="container">
+    <p className="heading">Sign In HIREMEHORIZON</p>
+    {/* {% if error_message %}
+  <p class="errormessage">{{ error_message }}</p>
+  {% endif %} */}
+    <form method="post">
+      <input type="text" name="username" placeholder="Username" required="" />
+      <br />
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        required=""
+      />
+      <br />
+      <button className="login" type="submit">
+        Sign In
+      </button>
+    </form>
+    <p className="registercontent">Forgot your password?</p>
+  </div>
+</>
+    );
+}
+
+export default Login;
